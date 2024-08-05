0a98ae0c31d2c2ae7752f9599372f2199cae38ce
ZyanAlkanza <zyan.mujaddid@gmail.com>
2024-08-05 17:36:06 +0700

    inisasi project

---
+++ database/migrations/2024_08_05_101151_create_patients_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->char('no_rekam_medik', 6)->primary();
+            $table->string('nama_pasien');
+            $table->char('nik', 16)->unique();
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
---
+++ database/migrations/2024_08_05_101208_create_visits_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->char('no_rekam_medik', 6);
+            $table->foreign('no_rekam_medik')->references('no_rekam_medik')->on('patients')->onDelete('cascade');
+            $table->integer('jumlah_kunjungan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
